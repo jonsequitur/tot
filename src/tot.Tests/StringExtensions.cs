ef2ce85f3d5b432314b42419de47b3b6d1563ee7
Jon Sequeira <jonsequeira@gmail.com>
2020-08-29 10:52:02 -0700

    test fixes

--- src/tot.Tests/StringExtensions.cs
+++ src/tot.Tests/StringExtensions.cs
@@ -5,7 +5,7 @@ namespace tot.Tests
     public static class StringExtensions
     {
         public static string NormalizeLineEndings(this string value) => 
-            value.Replace("\r\n", NewLine)
+            value.Replace("\r\n", "\n")
                  .Replace("\n", NewLine);
     }
 }
